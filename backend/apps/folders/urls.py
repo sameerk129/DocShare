d7d2feb93280ae18f82e65ea77f59be2603a27c1
Sameer Kumar <sameer.k@greyorange.sg>
2018-05-27 03:12:20 +0530

    url reordering

--- backend/apps/folders/urls.py
+++ backend/apps/folders/urls.py
@@ -3,8 +3,8 @@ from django.conf.urls import url, include
 from .views import FoldersList, FolderNotesList, FolderNote, FolderObj
 
 urlpatterns = [
-    url(r'^$', FoldersList.as_view(), name='folders_list'),
-    url(r'(?P<folder_id>\d+)/$', FolderObj.as_view(), name='folder_obj'),
-    url(r'(?P<folder_id>\d+)/notes/$', FolderNotesList.as_view(), name='folder_notes'),
     url(r'^(?P<folder_id>\d+)/notes/(?P<note_id>\d+)/$', FolderNote.as_view(), name='folder_note'),
+    url(r'(?P<folder_id>\d+)/notes/$', FolderNotesList.as_view(), name='folder_notes'),
+    url(r'(?P<folder_id>\d+)/$', FolderObj.as_view(), name='folder_obj'),
+    url(r'^$', FoldersList.as_view(), name='folders_list'),
 ]
